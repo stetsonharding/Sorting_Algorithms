1b55053481fcc28d25e2390651c46811183502e9
stetson Harding <stetonharding@outlook.com>
2022-08-10 16:29:04 -0700

    Merge sort implimented.

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,4 +1,4 @@
-const { ArrowRight } = require("@material-ui/icons");
+
 
 /* Bubble Sort */
 /* Sorts an array by compairing two adjacent elements and swaps them if they are not in the intended order (increasing or decreasing)
@@ -27,11 +27,3 @@ function bubbleSort(arr) {
 
 bubbleSort(testArr);
 
-/* Merge Sort */
-
-/*Divide and conquer statergy, array is initially divided into two equal halves and then they are combined in a sorted manner. 
-We can think of it as a recursive algorithm that continuously splits the array in half until it cannot be further divided. 
-This means that if the array becomes empty or has only one element left, the dividing will stop, i.e. it is the base case to stop the recursion. 
-If the array has multiple elements, we split the array into halves and recursively invoke the merge sort on each of the halves. Finally,
- when both the halves are sorted, the merge operation is applied. Merge operation is the process of taking two smaller sorted arrays 
- and combining them to eventually make a larger one. */
---
+++ MergeSort.js
@@ -0,0 +1,45 @@
+/* Merge Sort */
+
+/*Divide and conquer statergy, array is initially divided into two equal halves and then they are combined in a sorted manner. 
+We can think of it as a recursive algorithm that continuously splits the array in half until it cannot be further divided. 
+This means that if the array becomes empty or has only one element left, the dividing will stop, i.e. it is the base case to stop the recursion. 
+If the array has multiple elements, we split the array into halves and recursively invoke the merge sort on each of the halves. Finally,
+ when both the halves are sorted, the merge operation is applied. Merge operation is the process of taking two smaller sorted arrays 
+ and combining them to eventually make a larger one. */
+
+
+//Splitting array into halfs.
+function mergeSort(arr) {
+    let middle = Math.floor(arr.length / 2);
+  
+    if (arr.length < 2) {
+      return arr;
+    }
+  
+    const left = arr.slice(0, middle);
+    const right = arr.splice(middle, arr.length - 1);
+  
+    return merge(mergeSort(left), mergeSort(right));
+  }
+
+
+
+ //Sorting the array
+function merge(leftArr, rightArr) {
+  let result = [];
+
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] < rightArr[0]) {
+      result.push(leftArr.shift());
+    } else {
+      result.push(rightArr.shift());
+    }
+  }
+
+  return [...result, ...leftArr, ...rightArr];
+}
+
+
+
+
+console.log(mergeSort([2, 1, 9, 4, 3]));
