1d797901ed9cb016c3db84a22299882e9aaef404
stetsonharding <stetson2001@msn.com>
2022-08-10 21:32:42 -0700

    merge sort comments

--- MergeSort.js
+++ MergeSort.js
@@ -7,28 +7,32 @@ If the array has multiple elements, we split the array into halves and recursive
  when both the halves are sorted, the merge operation is applied. Merge operation is the process of taking two smaller sorted arrays 
  and combining them to eventually make a larger one. */
 
-
 //Splitting array into halfs.
 function mergeSort(arr) {
-    let middle = Math.floor(arr.length / 2);
-  
-    if (arr.length < 2) {
-      return arr;
-    }
-  
-    const left = arr.slice(0, middle);
-    const right = arr.splice(middle, arr.length - 1);
-  
-    return merge(mergeSort(left), mergeSort(right));
+  //calculation for middle index, will round down if decimal is calculated.
+  let middle = Math.floor(arr.length / 2);
+
+  //breaks out of recurstion call
+  //base case
+  if (arr.length < 2) {
+    return arr;
   }
 
+  //life side of array
+  const left = arr.slice(0, middle);
+  //right side of array
+  const right = arr.splice(middle, arr.length - 1);
 
+  return merge(mergeSort(left), mergeSort(right));
+}
 
- //Sorting the array
+//Sorting the array
 function merge(leftArr, rightArr) {
   let result = [];
 
+  //condition for checking if arrays have elements
   while (leftArr.length && rightArr.length) {
+    //Checks what array at index 0 should be pushed into result array.
     if (leftArr[0] < rightArr[0]) {
       result.push(leftArr.shift());
     } else {
@@ -38,8 +42,5 @@ function merge(leftArr, rightArr) {
 
   return [...result, ...leftArr, ...rightArr];
 }
-
-
-
 
 console.log(mergeSort([2, 1, 9, 4, 3]));
