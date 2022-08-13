884a5f438619303c10100cf154dba8145d62b21f
stetsonharding <stetson2001@msn.com>
2022-08-12 20:34:51 -0700

    Inital Binary Search Instructions.

---
+++ BinarySearch.js
@@ -0,0 +1,14 @@
+/* Binary Search */
+
+/*Binary Search is a searching algorithm for finding an element's position in a sorted array. Binary search compares the target value to the middle element of the array. 
+If they are not equal, the half in which the target cannot lie is eliminated and the search continues on the remaining half, again taking the middle element 
+to compare to the target value, and repeating this until the target value is found. ONLY WORKS WHEN ARRAY IS SORTED*/
+
+function BinarySeach(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start < end) {}
+}
+
+BinarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
