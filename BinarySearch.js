f2176aaf56f25d98d0a3053641b0fffec050419e
stetsonharding <stetson2001@msn.com>
2022-08-13 09:51:39 -0700

    Bubble sort complete with comments.

--- BinarySearch.js
+++ BinarySearch.js
@@ -4,16 +4,30 @@
 If they are not equal, the half in which the target cannot lie is eliminated and the search continues on the remaining half, again taking the middle element 
 to compare to the target value, and repeating this until the target value is found. ONLY WORKS WHEN ARRAY IS SORTED*/
 
-function BinarySearch(arr) {
-  let left = 0;
-  let right = arr.length - 1;
+function BinarySearch(arr, target) {
+  //pointers to beggining of array and end.
+  let start = 0;
+  let end = arr.length - 1;
 
-  while (left < right) {
-    let mid = Math.floor(left + right / 2);
+  //While loop to continuously split array into half until value is found or you cant split anymore.
+  while (start <= end) {
+    //Spit array into half based on middle point.
+    let mid = Math.floor((start + end) / 2);
 
-    console.log(mid);
-    left++;
+    //If mid index is the target return mid
+    if (arr[mid] === target) {
+      return mid;
+      //look at left half of array and split
+    } else if (arr[mid] > target) {
+      end = mid - 1;
+      //look at right half of the array and split
+    } else {
+      start = mid + 1;
+    }
   }
+
+  //if value is not found return null.
+  return null;
 }
 
-BinarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log(BinarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7));
